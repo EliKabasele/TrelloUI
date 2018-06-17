2abbfa82d4bf75dffad6907ca3c74d7fc2b4eb83
Eli Kabasele <kabasele@w11k.de>
2018-06-17 11:46:25 +0200

    adds namespace [Trello]

---
+++ src/trello.d.ts
@@ -0,0 +1,102 @@
+export namespace Trello {
+
+  export interface TotalPerMember {
+    status: string;
+    disableAt: number;
+    warnAt: number;
+  }
+
+  export interface Boards {
+    id: string;
+    name: string;
+    totalPerMember: TotalPerMember;
+  }
+
+  export interface TotalPerMember2 {
+    status: string;
+    disableAt: number;
+    warnAt: number;
+  }
+
+  export interface Cards {
+    id: string;
+    name: string;
+    badges: object;
+    dueComplete: boolean;
+    due?: string;
+    desc: string;
+    idMembers: object;
+  }
+
+  export interface Orgs {
+    totalPerMember: TotalPerMember2;
+  }
+
+  export interface Limits {
+    boards: Boards;
+    orgs: Orgs;
+  }
+
+  export interface MarketingOptIn {
+    optedIn: boolean;
+    date: Date;
+  }
+
+  export interface Prefs {
+    sendSummaries: boolean;
+    minutesBetweenSummaries: number;
+    minutesBeforeDeadlineToNotify: number;
+    colorBlind: boolean;
+    locale: string;
+  }
+
+  export interface InAndOutbox {
+    0: object;
+    1: Comment[];
+  }
+
+  export interface Comment {
+    id: string;
+    data: object;
+    date: Date;
+    idMemberCreator: string;
+    type: string;
+  }
+
+  export interface User {
+    id: string;
+    avatarHash: string;
+    avatarUrl: string;
+    bio: string;
+    bioData?: any;
+    confirmed: boolean;
+    fullName: string;
+    idEnterprisesDeactivated: any[];
+    idPremOrgsAdmin: any[];
+    initials: string;
+    memberType: string;
+    products: any[];
+    status: string;
+    url: string;
+    username: string;
+    avatarSource: string;
+    email?: any;
+    gravatarHash: string;
+    idBoards: string[];
+    idEnterprise?: any;
+    idOrganizations: any[];
+    idEnterprisesAdmin: any[];
+    limits: Limits;
+    loginTypes?: any;
+    marketingOptIn: MarketingOptIn;
+    messagesDismissed: any[];
+    oneTimeMessagesDismissed: string[];
+    prefs: Prefs;
+    trophies: any[];
+    uploadedAvatarHash: string;
+    uploadedAvatarUrl: string;
+    premiumFeatures: any[];
+    idBoardsPinned?: any;
+  }
+
+}
